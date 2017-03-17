d4ef26903989233e2cec136a5f307ede13c73163
Daniel Werdermann <dwerdermann@web.de>
2017-03-17 14:52:11 +0100

    Update check_mountpoints.sh

--- check_mountpoints.sh
+++ check_mountpoints.sh
@@ -18,7 +18,7 @@
 # Check if all specified nfs/cifs/davfs mounts exist and if they are correct implemented.
 # That means we check /etc/fstab, the mountpoints in the filesystem and if they
 # are mounted. It is written for Linux, uses proc-Filesystem and was tested on
-# Debian, OpenSuse 10.1 10.2 10.3 11.0, SLES 10.1 11.1, RHEL 5 6 7, CentOS 5 6 7 and solaris
+# Debian, OpenSuse 10.1 10.2 10.3 11.0, SLES 10.1 11.1, RHEL/CentOS 5 6 7, FreeBSD and solaris
 #
 # @author: Daniel Werdermann / dwerdermann@web.de
 # @projectsite: https://github.com/echocat/nagios-plugin-check_mountpoints
